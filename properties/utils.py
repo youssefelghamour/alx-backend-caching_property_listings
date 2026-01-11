57ecf845f6dd606649f9324acaeac074f794a859
youssef <youssefelghamour@gmail.com>
2026-01-11 15:35:23 +0100

    2. Low-Level Caching for Property Queryset

--- properties/utils.py
+++ properties/utils.py
@@ -6,6 +6,6 @@ def get_all_properties():
     """Function to get all properties with caching"""
     properties = cache.get('all_properties')
     if not properties:
-        properties = list(Property.objects.values())
+        properties = list(Property.objects.all().values())
         cache.set('all_properties', properties, 3600)  # Cache for 1 hour
     return properties
